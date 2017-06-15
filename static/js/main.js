7d53a9344a8174a3a8548adf81bc2b2ca214b81d
Daniel Byun <db186061@teradata.com>
2017-06-15 12:03:36 -0700

    HACKATHON FINAL VERSION

--- static/js/main.js
+++ static/js/main.js
@@ -1,68 +1,273 @@
-let dbsc_file = "../../"
+
+var firstPoint = secondPoint = null;
+
+var handle_feature_usage = function(feature_data) {
+	var add_list = feature_data['add'];
+	var rm_list = feature_data['rm'];
+
+	ret_html = "<ul class=\"list-group\">";
+	for(var i=0; i < add_list.length; i++) {
+		ret_html += "<li class=\"list-group-item-success\">" + add_list[i] + "</li>";
+	}
+	for(var i=0; i < rm_list.length; i++) {
+		ret_html += "<li class=\"list-group-item-danger\">" + rm_list[i] + "</li>";
+	}
+	ret_html += "</ul>";
+	return ret_html;
+}
+
+var handle_dbsc = function(dbsc_data) {
+	ret_html = "<div class=\"card-deck\">";
+	for(var i=0; i < dbsc_data.length; i++) {
+		ret_html += "<div class=\"card\">";
+		ret_html += "<div class=\"card-header\">" + dbsc_data[i]["name"] + "</div>";
+		ret_html += "<div class=\"card-block\">";
+		ret_html += "<ul class=\"list-group\">";
+		ret_html += "<li class=\"list-group-item-success\">" + dbsc_data[i]["new"] + "</li>";
+		ret_html += "<li class=\"list-group-item-danger\">" + dbsc_data[i]["old"] + "</li>";
+		ret_html += "</ul></div></div>"
+	}
+	return ret_html + "</div>";
+}
+
+var handle_tasm = function(tasm_data) {
+	var add_list = tasm_data['add'];
+	var rm_list = tasm_data['rm'];
+
+	ret_html = "<ul class=\"list-group\">";
+	var i = j = 0;
+	while(i < add_list.length && j < add_list.length) {
+		ret_html += "<li class=\"list-group-item-success\">" + add_list[i] + "</li>";
+		if(String(add_list[i]).split("::")[0] == String(rm_list[j]).split("::")[0]) {
+			ret_html += "<li class=\"list-group-item-danger\">" + rm_list[j] + "</li>";
+			++j;
+		}
+		++i;
+	}
+	while(i < add_list.length) {
+		ret_html += "<li class=\"list-group-item-success\">" + add_list[i++] + "</li>";
+	}
+	while(j < rm_list.length) {
+		ret_html += "<li class=\"list-group-item-danger\">" + rm_list[j++] + "</li>";
+	}
+	ret_html += "</ul>";
+	return ret_html;
+}
 
 $(document).ready(function() {
-	$('#scatterplot').highcharts({
+	Highcharts.theme = {
+	  "colors": ["#A9CF54", "#C23C2A", "#FFFFFF", "#979797", "#FBB829"],
+	  "chart": {
+	    "backgroundColor": "#242F39",
+	    "style": {
+	      "color": "white"
+	    }
+	  },
+	  "legend": {
+	    "enabled": true,
+	    "align": "right",
+	    "verticalAlign": "bottom",
+	    "itemStyle": {
+	      "color": "#C0C0C0"
+	    },
+	    "itemHoverStyle": {
+	      "color": "#C0C0C0"
+	    },
+	    "itemHiddenStyle": {
+	      "color": "#444444"
+	    }
+	  },
+	  "title": {
+	    "text": {},
+	    "style": {
+	      "color": "#FFFFFF"
+	    }
+	  },
+	  "tooltip": {
+	    "backgroundColor": "#1C242D",
+	    "borderColor": "#1C242D",
+	    "borderWidth": 1,
+	    "borderRadius": 0,
+	    "style": {
+	      "color": "#FFFFFF"
+	    }
+	  },
+	  "subtitle": {
+	    "style": {
+	      "color": "#666666"
+	    }
+	  },
+	  "xAxis": {
+	    "gridLineColor": "#2E3740",
+	    "gridLineWidth": 1,
+	    "labels": {
+	      "style": {
+	        "color": "#525252"
+	      }
+	    },
+	    "lineColor": "#2E3740",
+	    "tickColor": "#2E3740",
+	    "title": {
+	      "style": {
+	        "color": "#FFFFFF"
+	      },
+	      "text": {}
+	    }
+	  },
+	  "yAxis": {
+	    "gridLineColor": "#2E3740",
+	    "gridLineWidth": 1,
+	    "labels": {
+	      "style": {
+	        "color": "#525252"
+	      },
+	      "lineColor": "#2E3740",
+	      "tickColor": "#2E3740",
+	      "title": {
+	        "style": {
+	          "color": "#FFFFFF"
+	        },
+	        "text": {}
+	      }
+	    }
+	  }
+	};
+	Highcharts.setOptions(Highcharts.theme);
+
+	var scatterplot = $('#scatterplot').highcharts({
 	    chartID: "scatter1",
 			title: {
-					text: "Query Analyzer"
+				text: "Query Guru",
+				align: "center"
 			},
-        tooltip: {
-                formatter: function() {
-                    return  '<b>'+this.series.name+' </b>' +
-                        Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', this.x)
-                    + ' date, ' + this.y + ' run duration';
-                }
-        },
+	    tooltip: {
+        formatter: function() {
+					var ret = "<b>" + this.series.name + "</b><br/>";
+					ret += "StartTime: " + Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', this.x) + "<br/>";
+					ret += "Duration: " + this.y + " minutes<br/>";
+					ret += "Run ID: " + this.point.run_id + "<br/>";
+					return ret;
+        }
+      },
 	    chart: {
-	        type: 'scatter',
-	        zoomType: 'xy',
-					width: 1000,
-					height: 400
+        type: 'scatter',
+        zoomType: 'xy',
+				height: screen.height*0.5
 	    },
 	    xAxis: {
-			type: 'datetime',
-            labels: {
-                formatter: function() {
-                    return Highcharts.dateFormat('%a %d %b %H %M', this.value);
-                }
-            }
+				title: {
+					text: "Run time"
+				},
+				type: 'datetime',
+        	labels: {
+	          formatter: function() {
+	            return Highcharts.dateFormat('%b %d %H:%M', this.value);
+	          }
+      	}
 	    },
 	    yAxis: {
-	        title: {
-	            text: "Run duration (s)"
-	        }
+        title: {
+          text: "Run duration (min)"
+        }
 	    },
 			legend: {
-		        layout: 'vertical',
-		        align: 'right',
-		        verticalAlign: 'top',
-		        floating: false,
-		        backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFF',
-		        borderWidth: 1
+        layout: 'vertical',
+        align: 'left',
+        verticalAlign: 'top',
+        floating: false,
+        backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFF',
+        borderWidth: 1
 	    },
 			plotOptions: {
         scatter: {
-            marker: {
-                radius: 4,
-                states: {
-                    hover: {
-                        enabled: true,
-                        lineColor: 'rgb(100,100,100)'
-                    }
-                }
-            },
+					lineWidth:2,
+          marker: {
+            radius: 4,
             states: {
-                hover: {
-                    marker: {
-                        enabled: false
-                    }
-                }
-            },
+              hover: {
+                enabled: true,
+                lineColor: 'rgb(100,100,100)'
+              }
+            }
+          },
+          states: {
+            hover: {
+              marker: {
+                enabled: true
+              }
+            }
+          },
         },
 				series: {
-					events: {
-						click: function(e) {
-							
+					visible: false,
+					allowPointSelect: true,
+					marker: {
+          	states: {
+            	select: {
+                fillColor: 'red',
+                lineWidth: 0
+              }
+          	}
+        	},
+					point: {
+						events: {
+							click: function(e) {
+								var details_fn = function(obj) {
+									var details_list = "<ul>";
+									details_list += "<li>Collect Timestamp: " + Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', obj.x) + "</li>";
+									details_list += "<li>Run Duration: " + obj.y + " minutes</li>";
+									details_list += "<li>Run ID: " + obj.run_id + "</li>";
+									details_list += "<li>Run Number: " + obj.run_num + "</li>";
+									details_list += "</ul>"
+									return details_list;
+								}
+								if(firstPoint == null) {
+									firstPoint = this;
+									$('#query1').text(this.series.name + " (" + this.run_num + ")");
+									$('#query1_details').html(details_fn(this));
+								}
+								else if(secondPoint == null) {
+									secondPoint = this;
+									$('#query2').text(this.series.name + " (" + this.run_num + ")");
+									$('#query2_details').html(details_fn(this));
+									$('#dbsc').html("<i class=\"fa fa-spinner fa-spin\" style=\"font-size:24px\"></i>");
+									$('#feature').html("<i class=\"fa fa-spinner fa-spin\" style=\"font-size:24px\"></i>");
+									$('#tasm').html("<i class=\"fa fa-spinner fa-spin\" style=\"font-size:24px\"></i>");
+
+									$.post("/tasm", {
+											tasm_file1: firstPoint.tasm_file,
+											tasm_file2: secondPoint.tasm_file
+									}, function(data) {
+										$('#tasm').html(handle_tasm(data));
+									});
+
+									$.post("/feature", {
+											run_id1: firstPoint.run_id,
+											run_id2: secondPoint.run_id
+									}, function(data) {
+										$('#feature').html(handle_feature_usage(data));
+									});
+
+									$.post("/dbsc", {
+											run_id1: firstPoint.run_id,
+											run_id2: secondPoint.run_id
+									}, function(data) {
+										$('#dbsc').html(handle_dbsc(data));
+									});
+
+								}
+								else {
+									secondPoint = null;
+									firstPoint = this;
+									$('#query2').text("SELECT A POINT");
+									$('#query2_details').text("");
+									$('#query1').text(this.series.name + " (" + this.run_num + ")");
+									$('#query1_details').html(details_fn(this));
+									$('#dbsc').html("");
+									$('#feature').html("");
+									$('#tasm').html("");
+								}
+							}
 						}
 					}
 				}
--- webapp.py
+++ webapp.py
@@ -1,93 +1,211 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 import teradata
 import time
-import json
 import os
+import json
+import random
 
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
-json_path = os.path.realpath(os.path.join("static", "js"))
+
+class JSONDelta:
+    def __init__(self, json1, json2):
+        self.value_delimiter = "::"
+        self.key_delimiter = "/"
+        self.dict_delimiter = "d"
+        self.list_delimiter = "l"
+        self.json1 = json1
+        self.json2 = json2
+
+    def diff(self):
+        l1 = self.flatten("", self.json1)
+        l2 = self.flatten("", self.json2)
+        return self.diffdict(l1, l2)
+
+    def flatten(self, accpath, obj):
+        flattened_list = []
+        if isinstance(obj, dict):
+            for k, v in obj.items():
+                newpath = accpath + self.key_delimiter + str(k)
+                flattened_list += self.flatten(newpath, v)
+        elif isinstance(obj, list):
+            newpath = accpath + self.key_delimiter
+            for x in obj:
+                if isinstance(x, list):
+                    flattened_list += self.flatten(newpath + 'l', x)
+                else:
+                    flattened_list += self.flatten(newpath, x)
+        else:
+            return [accpath + self.value_delimiter + str(obj)]
+        return flattened_list
+
+    def diffdict(self, l1, l2):
+        sl1 = sorted(l1)
+        sl2 = sorted(l2)
+        added_list = [x for x in sl1 if x not in sl2]
+        rmved_list = [x for x in sl2 if x not in sl1]
+        return {'add': added_list, 'rm': rmved_list}
 
 
-def get_dbsc(con):
-    sql_dbsc_id = "select distinct run_id from TQST_TEST_PROFILE_DB.hack_runtime"
-    with con.cursor() as cursor:
-        cursor.execute(sql_dbsc_id)
-        res_id = cursor.fetchall()
-        all_data = {}
-        for run_id in res_id:
-            dbsc_data = {}
-            sql_dbsc_data = """select GroupName, FieldNum, FieldName, FieldValue from TQST_TEST_PROFILE_DB.hack_dbscontrol
-                               where run_id = {} and GroupName <> '2'""".format(run_id[0])
-            cursor.execute(sql_dbsc_data)
-            res_data = cursor.fetchall()
-            if not res_data:
-                raise ValueError("There is no data")
-            for r in res_data:
-                k = "{0}  {1}[{2}]".format(r[0].strip(), r[2].strip(), r[1])
-                dbsc_data[k] = r[3].split("(")[0].strip()
-            all_data[int(run_id[0])] = dbsc_data
-    return all_data
+@app.route('/tasm', methods=['POST'])
+def tasm():
+    file1 = str(request.form['tasm_file1'])
+    file2 = str(request.form['tasm_file2'])
+    return jsonify(get_tasm_diff(file1, file2))
 
 
-def get_query_data(con):
-    rows = con.execute(
-        ("select * from TQST_TEST_PROFILE_DB.hack_runtime order by sql_name asc, run_num;"))
-    data = {}
-    for row in rows:
-        sql_name = row['sql_name']
-        run_num = row['run_num']
-        run_id = int(row['run_id'])
-        run_duration = float(row['run_duration'])
-        StartTime = time.mktime(row['startTime'].timetuple()) * 1000
-        label = row['label']
-        try:
-            unique_query_data = data[sql_name]
-            unique_query_data[run_num] = {
-                'run_id': run_id,
-                'run_duration': run_duration,
-                'StartTime': StartTime,
-                'label': label
-            }
-        except KeyError:
-            data[sql_name] = {
-                run_num: {
+@app.route('/dbsc', methods=['POST'])
+def dbsc():
+    run_id1 = request.form['run_id1']
+    run_id2 = request.form['run_id2']
+    return jsonify(get_dbsc_diff(run_id1, run_id2))
+
+
+@app.route('/feature', methods=['POST'])
+def feature():
+    run_id1 = request.form['run_id1']
+    run_id2 = request.form['run_id2']
+    return jsonify(get_feature_usage_diff(run_id1, run_id2))
+
+
+def get_tasm_diff(file1, file2):
+    time.sleep(3)
+    with open(file1) as of, open(file2) as nf:
+        json1 = json.load(of)
+        json2 = json.load(nf)
+    return JSONDelta(json1, json2).diff()
+
+
+def get_feature_usage_diff(run_id1, run_id2):
+    feature_data = {'add': [], 'rm': []}
+    udaExec = teradata.UdaExec(appName="qanalyzer", version="1.0")
+    with udaExec.connect(method="odbc",
+                         system="italy",
+                         username="tqpe_uda_solutions_admin",
+                         password="tqpe_uda_solutions_admin") as con:
+        sql_query = \
+            """
+            select (select trim(feature_name) from  TQST_TEST_PROFILE_DB.hack_features where run_id={0}
+            EXCEPT
+            select trim(feature_name) from TQST_TEST_PROFILE_DB.hack_features  where run_id={1}) as addition
+            ,
+            (select trim(feature_name) from TQST_TEST_PROFILE_DB.hack_features  where run_id={1}
+            EXCEPT
+            select trim(feature_name) from TQST_TEST_PROFILE_DB.hack_features where run_id={0}) as removed;
+            """.format(run_id1, run_id2)
+
+        # ("(select trim(feature_name) from TQST_TEST_PROFILE_DB.hack_features "
+        #  "where run_id={0} EXCEPT select trim(feature_name) "
+        #  "from TQST_TEST_PROFILE_DB.hack_features where run_id={1}) as addition, "
+        #  "(select trim(feature_name) from TQST_TEST_PROFILE_DB.hack_features "
+        #  "where run_id={1} EXCEPT select trim(feature_name) "
+        #  "from TQST_TEST_PROFILE_DB.hack_features where run_id={0}) as removed;").format(run_id1, run_id2)
+
+        res_data = con.execute(sql_query)
+        for r in res_data:
+            if r['addition']:
+                feature_data['add'].append(r['addition'])
+            elif r['removed']:
+                feature_data['rm'].append(r['removed'])
+    return feature_data
+
+
+def get_dbsc_diff(run_id1, run_id2):
+    dbsc_data = []
+    udaExec = teradata.UdaExec(appName="qanalyzer", version="1.0")
+    with udaExec.connect(method="odbc",
+                         system="italy",
+                         username="tqpe_uda_solutions_admin",
+                         password="tqpe_uda_solutions_admin") as con:
+        sql_dbsc_data = \
+            ("select trim(a.fieldname) as name, trim(a.fieldvalue) as aval, trim(b.fieldvalue) as bval from "
+             "(select * from TQST_TEST_PROFILE_DB.hack_dbscontrol where run_id={}) a, "
+             "(select * from TQST_TEST_PROFILE_DB.hack_dbscontrol where run_id={}) b "
+             "where a.groupName = b.groupName and a.FieldNum = b.FieldNum and a.FieldName = b.FieldName "
+             "and a.fieldvalue <> b.fieldvalue;").format(run_id1, run_id2)
+        res_data = con.execute(sql_dbsc_data)
+        for r in res_data:
+            dbsc_data.append({'name': str(r['name']).strip(),
+                              'new': str(r['aval']).strip(),
+                              'old': str(r['bval']).strip()})
+    return dbsc_data
+
+
+def get_query_data():
+    udaExec = teradata.UdaExec(appName="qanalyzer", version="1.0")
+    with udaExec.connect(method="odbc",
+                         system="italy",
+                         username="tqpe_uda_solutions_admin",
+                         password="tqpe_uda_solutions_admin") as con:
+        rows = con.execute(
+            ("select * from TQST_TEST_PROFILE_DB.hack_runtime order by sql_name asc, run_num;"))
+        data = {}
+
+        json_path = os.path.realpath(os.path.join("static", "json"))
+        files_list = [
+            "tdwmdmp_03232017_1639.json",
+            "tdwmdmp_03232017_1714.json",
+            "tdwmdmp_06122017_1403.json",
+            "tdwmdmp_06132017_1430.json",
+            "tdwmdmp_06142017_1454.json"
+        ]
+        for row in rows:
+            sql_name = row['sql_name']
+            run_num = row['run_num']
+            run_id = str(row['run_id'])
+            run_duration = float(row['run_duration']) / 60.0
+            StartTime = time.mktime(row['startTime'].timetuple()) * 1000
+            label = row['label']
+            try:
+                unique_query_data = data[sql_name]
+                unique_query_data[run_num] = {
                     'run_id': run_id,
                     'run_duration': run_duration,
                     'StartTime': StartTime,
-                    'label': label
+                    'label': label,
+                    'tasm_file': os.path.join(json_path, random.choice(files_list))
                 }
-            }
+            except KeyError:
+                data[sql_name] = {
+                    run_num: {
+                        'run_id': run_id,
+                        'run_duration': run_duration,
+                        'StartTime': StartTime,
+                        'label': label,
+                        'tasm_file': os.path.join(json_path, random.choice(files_list))
+                    }
+                }
 
     series = []
     for k_sql_name, sql_name_dict in data.items():
         points = []
         for k_run_num, run_num_dict in sql_name_dict.items():
-            points.append([run_num_dict['StartTime'], run_num_dict['run_duration']])
+            points.append({
+                'x': run_num_dict['StartTime'],
+                'y': run_num_dict['run_duration'],
+                'run_num': int(k_run_num),
+                'run_id': run_num_dict['run_id'],
+                'label': run_num_dict['label'],
+                'tasm_file': run_num_dict['tasm_file']
+            })
 
         series.append({
             "name": k_sql_name,
             "color": "rgba(71, 206, 244, 0.5)",
-            "data": points,
-            "visible": "false"
+            "data": points
         })
-    return series
+
+        sorted_series = sorted(series, key=lambda k: k['name'])
+        for i in range(len(sorted_series)):
+            sorted_series[i]["legendIndex"] = i
+    return sorted_series
 
 
 @app.route("/")
 @app.route("/index")
 def index():
-    udaExec = teradata.UdaExec(appName="adjfkl", version="1.0")
-    con = udaExec.connect(method="odbc", system="italy",
-                          username="tqpe_uda_solutions_admin", password="tqpe_uda_solutions_admin")
-    dbsc_data = get_dbsc(con)
-    series = get_query_data(con)
-
-    dbsc_output_path = os.path.join(json_path, "dbsc_data.json")
-    with open(dbsc_output_path, 'w') as fp:
-        json.dump(dbsc_data, fp)
-
+    series = get_query_data()
     return render_template('index.html', series=series)
 
 
